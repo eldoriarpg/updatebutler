7d10b5845df88129c92e1807b5b1fd7b630173c9
Florian Fülling <mail@code-fox.de>
2020-10-16 20:04:18 +0200

    rename plain text command

--- src/main/java/de/eldoria/updatebutler/config/commands/PlainCommand.java
+++ src/main/java/de/eldoria/updatebutler/config/commands/PlainCommand.java
@@ -3,11 +3,11 @@ package de.eldoria.updatebutler.config.commands;
 import com.google.gson.annotations.Expose;
 import net.dv8tion.jda.api.entities.TextChannel;
 
-public class PlainTextCommand extends UserCommand {
+public class PlainCommand extends UserCommand {
     @Expose
     private final String text;
 
-    public PlainTextCommand(String command, String text) {
+    public PlainCommand(String command, String text) {
         super(command);
         this.text = text;
     }

72ccb646ce02ccb5934b50df37ef8f006d6ab2b1
Florian Fülling <mail@chojo.de>
2021-01-15 14:48:27 +0100

    list world names instead of count. I see you spaces 👀

--- src/main/java/de/eldoria/updatebutler/api/debug/data/ServerMetaData.java
+++ src/main/java/de/eldoria/updatebutler/api/debug/data/ServerMetaData.java
@@ -9,10 +9,10 @@ import java.util.stream.Collectors;
 public class ServerMetaData {
     protected String version;
     protected int currentPlayers;
-    protected int loadedWorlds;
+    protected String[] loadedWorlds;
     protected PluginMetaData[] plugins;
 
-    protected ServerMetaData(String version, int currentPlayers, int loadedWorlds, PluginMetaData[] plugins) {
+    protected ServerMetaData(String version, int currentPlayers, String[] loadedWorlds, PluginMetaData[] plugins) {
         this.version = version;
         this.currentPlayers = currentPlayers;
         this.loadedWorlds = loadedWorlds;
@@ -22,7 +22,7 @@ public class ServerMetaData {
     @Override
     public String toString() {
         String plugins = Arrays.stream(this.plugins).map(m -> m.toString(2)).collect(Collectors.joining("\n"));
-        return String.format("version: %s%ncurrentPlayers: %d%nloadedWorlds%d%npluginCount: %d%nplugins:%n%s",
-                version, currentPlayers, loadedWorlds, this.plugins.length, plugins);
+        return String.format("version: %s%ncurrentPlayers: %d%nloadedWorlds: [%s]%npluginCount: %d%nplugins:%n%s",
+                version, currentPlayers, String.join(", ", loadedWorlds), this.plugins.length, plugins);
     }
 }
